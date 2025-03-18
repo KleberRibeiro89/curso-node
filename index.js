f4e275fd70389b89d6396dc52a31295d1573945c
Kleber Ribeiro <kleber.ribeiro89@gmail.com>
2025-03-18 16:45:09 -0300

    Criação da Api

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const http = require('http')
+const url = require('url')
 
 http.createServer((request, response) => {
-    response.write('hello wolrd 2!');
+    if (request.method === 'POST') {
+        response.write('hello wolrd 22!');
+    }
+
+    const parsedUrl = url.parse(request.url, true);
+    const queryParams = parsedUrl.query;
+    const queryParamspathName = parsedUrl.pathname;
+
+    if (queryParamspathName === '/favicon.ico') {
+        response.writeHead(404);
+        response.end();
+        return;
+    }
+
+    
+    if (request.method === 'GET') {
+        response.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+        console.log(queryParams);
+
+        response.write('hello wolrd 2 get!\r\n');
+        response.write(`O parâmetro é: ${queryParams.id}`);
+
+    }
     response.end();
+
 }).listen(3000);
 
