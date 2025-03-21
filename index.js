6391846710923cffeb61ee4d75dd1b9ddc71b303
Kleber Ribeiro <kleber.ribeiro89@gmail.com>
2025-03-21 17:56:40 -0300

    express ok

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
-const http = require('http')
-const url = require('url')
+const express = require('express')
 
-http.createServer((request, response) => {
-    if (request.method === 'POST') {
-        response.write('hello wolrd 22!');
-    }
+const app = express();
+const porta = 3000;
+app.get('/', (req, res) => {
+    res.send('Hello World ç á test')
+})
 
-    const parsedUrl = url.parse(request.url, true);
-    const queryParams = parsedUrl.query;
-    const queryParamspathName = parsedUrl.pathname;
-
-    if (queryParamspathName === '/favicon.ico') {
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-
-    
-    if (request.method === 'GET') {
-        response.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-        console.log(queryParams);
-
-        response.write('hello wolrd 2 get!\r\n');
-        response.write(`O parâmetro é: ${queryParams.id}`);
-
-    }
-    response.end();
-
-}).listen(3000);
-
+app.listen(porta,  ()=>{
+    console.log(`servidor rodando na porta ${porta}`)
+})
